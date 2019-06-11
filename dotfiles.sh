ef6c7f80d2ea0201dbb2b9cb391d14249e1c43b2
Devin Clark <dclarkdesign@gmail.com>
2019-06-10 20:39:43 -0500

    tmuxinator

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,4 +1,7 @@
-ln -s $(pwd)/.tmux.conf ~/.tmux.conf
-ln -s $(pwd)/ubuntu/.bash_profile ~/.bash_profile
-ln -s $(pwd)/ubuntu/.bashrc ~/.bashrc
-ln -s $(pwd)/.gitconfig ~/.gitconfig
+#!/usr/bin/env bash
+
+ln -s "$(pwd)/.tmux.conf" ~/.tmux.conf
+ln -s "$(pwd)/ubuntu/.bash_profile" ~/.bash_profile
+ln -s "$(pwd)/ubuntu/.bashrc" ~/.bashrc
+ln -s "$(pwd)/.gitconfig" ~/.gitconfig
+ln -s "$(pwd)/.tmuxinator" ~/.tmuxinator
