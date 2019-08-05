f69023d39e6d44f6968392534a2bf6f0b448e126
Devin Clark <dclarkdesign@gmail.com>
2019-08-05 10:57:51 -0500

    Fix looping issue with directory symlinks.

--- dotfiles.sh
+++ dotfiles.sh
@@ -4,5 +4,10 @@ ln -s "$(pwd)/.tmux.conf" ~/.tmux.conf
 ln -s "$(pwd)/ubuntu/.bash_profile" ~/.bash_profile
 ln -s "$(pwd)/ubuntu/.bashrc" ~/.bashrc
 ln -s "$(pwd)/.gitconfig" ~/.gitconfig
-ln -s "$(pwd)/.tmuxinator" ~/.tmuxinator
-ln -s $(pwd)/sublime ~/.config/sublime-text-3/Packages/User
+
+# Check to make sure directory does not already exist
+# before creating a symlink for it. This prevents a
+# looping issue with directory symlinks.
+if [ ! -d ~/.tmuxinator ]; then ln -s "$(pwd)/.tmuxinator" ~/.tmuxinator; fi
+
+if [ ! -d ~/.config/sublime-text-3/Packages/User ]; then ln -s "$(pwd)/sublime" ~/.config/sublime-text-3/Packages/User; fi
