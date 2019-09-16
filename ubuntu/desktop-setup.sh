d24f9be2732728de603034a2d001eec65cb58096
Devin Clark <dclarkdesign@gmail.com>
2019-09-16 11:29:11 -0500

    tmux stuff

--- ubuntu/desktop-setup.sh
+++ ubuntu/desktop-setup.sh
@@ -34,7 +34,7 @@ echo "Installing Packages"
 # make exfat usb drives work
 sudo apt install -y exfat-fuse exfat-utils
 
-sudo apt install -y tree htop git curl tig shellcheck tmux xclip
+sudo apt install -y tree htop git curl tig shellcheck tmux xclip urlview
 
 #sudo apt install -y vlc bleachbit
 
@@ -68,6 +68,8 @@ curl https://sh.rustup.rs -sSf | sh
 
 gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable --auto-dotfiles
+
+sudo usermod -a -G rvm "$USER"
 
 rvm install 2.6.3
 rvm --default use 2.6.3
